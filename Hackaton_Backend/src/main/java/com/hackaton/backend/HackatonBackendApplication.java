3b74f70914daf5c3855dc149c900b6ac31835c3b
Juan Pablo Rodriguez Pedraos <juancursosrodriguez@gmail.com>
2021-11-17 23:31:24 -0500

    Primer commit

---
+++ Hackaton_Backend/src/main/java/com/hackaton/backend/HackatonBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hackaton.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackatonBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackatonBackendApplication.class, args);
+	}
+
+}
---
+++ Hackaton_Backend/src/test/java/com/hackaton/backend/HackatonBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackaton.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackatonBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
